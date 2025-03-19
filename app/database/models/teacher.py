99ccbd3a7a58699ad48f96d9df76432ba39878ff
cofeek-codes <kormyshev11@mail.ru>
2025-03-19 15:27:41 +0500

    dto classes created

--- app/database/models/teacher.py
+++ app/database/models/teacher.py
@@ -1,7 +1,6 @@
 from sqlmodel import SQLModel, Field
 
-class Teacher(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class TeacherBase(SQLModel):
     name: str = Field(index=True)
     subject: str = Field(index=True)
     lessons_amount: int = Field(index=True)
@@ -9,3 +8,19 @@ class Teacher(SQLModel, table=True):
     teacher_courses_amount: int = Field(index=True)
     positive_rewiews_amount: float = Field(index=True)
     
+class Teacher(TeacherBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+class TeacherCreate(TeacherBase):
+    pass
+
+class TeacherUpdate(TeacherBase):
+    name: str | None = None
+    subject: str | None = None
+    lessons_amount: int | None = None
+    grade_point_avg: float | None = None
+    teacher_courses_amount: int | None = None
+    positive_rewiews_amount: float | None = None
+        
+class TeacherPublic(TeacherBase):
+    id: int
